1b38a5ffe6bfdaf39d8dbedec782ba71bbcafae5
dianabarrientoss <dbarrientos12@comunidad.unam.mx>
2025-11-06 12:39:22 -0600

    delete_deterministic_model

--- Deterministic_Model.py
+++
@@ -1,9 +0,0 @@
-#Import libraries
-import numpy as np
-from scipy.integrate import odeint
-import matplotlib.pyplot as plt
-from scipy.optimize import fsolve
-from PyDSTool import *
-from PyDSTool.Toolbox import phaseplane as pp
-import PyDSTool as dst
-from random import random
